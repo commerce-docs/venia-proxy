b921f9b363413aef9eda1025320a9f883b14de79
Bruce Denham <bdenham@adobe.com>
2023-09-15 10:49:34 -0500

    chore: add copyright notices

--- index.jsx
+++ index.jsx
@@ -1,3 +1,7 @@
+/* Copyright 2023 Adobe. All Rights Reserved.
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying it. */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
--- proxy-server.js
+++ proxy-server.js
@@ -1,3 +1,7 @@
+/* Copyright 2023 Adobe. All Rights Reserved.
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying it. */
+
 import express from "express";
 import cors from "cors";
 import { createProxyMiddleware } from "http-proxy-middleware";
